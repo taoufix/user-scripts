5f94268925394d16ade34ce21b86e2f310fc1bdc
Taoufik El Aoumari <taoufix@gmail.com>
2018-05-17 13:46:32 +0200

    Create 20min-motfleches-afficher-force.user.js

---
+++ 20min-motfleches-afficher-force.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Mot flêchés force
+// @namespace    http://taoufix.com/20min-motfleches-afficher-force
+// @version      0.1
+// @updateURL    https://github.com/taoufix/user-scripts/raw/master/20min-motfleches-afficher-force.user.js
+// @author       taoufix
+// @match        http://rcijeux.fr/game/20minutes/mfleches?id=*
+// @grant        none
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function getDate() {
+        return window.location.href.split('=')[1];
+    }
+
+    $.get("http://rcijeux.fr/drupal_game/20minutes/grids/"+getDate()+".mfj", function( data ) {
+       var force = data.split("\n")[3].split('"')[1];
+        $( "#game-name" ).html("Force: " + force);
+    });
+
+})();
