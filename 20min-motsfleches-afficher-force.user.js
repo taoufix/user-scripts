b0912c09a4ef11d741bc625012aaf704a893f489
Taoufik El Aoumari <taoufix@gmail.com>
2018-06-01 15:16:24 +0200

    Update 20min-motsfleches-afficher-force.user.js

--- 20min-motsfleches-afficher-force.user.js
+++ 20min-motsfleches-afficher-force.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         20min Mots flêchés afficher force
 // @namespace    http://taoufix.com/20min-motsfleches-afficher-force
-// @version      0.1
+// @version      1.0.1
 // @updateURL    https://github.com/taoufix/user-scripts/raw/master/20min-motsfleches-afficher-force.user.js
 // @author       taoufix
-// @match        http://rcijeux.fr/game/20minutes/mfleches?id=*
+// @match        http*://rcijeux.fr/game/20minutes/mfleches?id=*
+// @match        http*://www.rcijeux.fr/game/20minutes/mfleches?id=*
 // @grant        none
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // ==/UserScript==
@@ -12,13 +13,24 @@
 (function() {
     'use strict';
 
+    console.log("---- User script [20min mfleches] ----");
+
+    var DEBUG = false;
     function getDate() {
         return window.location.href.split('=')[1];
     }
 
-    $.get("http://rcijeux.fr/drupal_game/20minutes/grids/"+getDate()+".mfj", function( data ) {
-       var force = data.split("\n")[3].split('"')[1];
+    $.get(window.location.origin+"/drupal_game/20minutes/grids/"+getDate()+".mfj")
+     .done(function( data ) {
+        var force = data.split("\n")[3].split('"')[1];
+        if (DEBUG) {
+            console.log("force", force);
+        }
         $( "#game-name" ).html("Force: " + force);
-    });
+     })
+     .fail(function(err) {
+        console.log("---- ERROR ----");
+        console.log(err);
+     });
 
 })();
