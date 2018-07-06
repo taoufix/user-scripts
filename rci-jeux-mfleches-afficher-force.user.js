17f63aa9e9c546406a0665cb0ad41ce3cae10be3
Taoufik El Aoumari <taoufix@gmail.com>
2018-07-06 11:25:03 +0200

    Added piano button

--- rci-jeux-mfleches-afficher-force.user.js
+++ rci-jeux-mfleches-afficher-force.user.js
@@ -2,7 +2,7 @@
 // @name         Mots flêchés RCI Jeux - Afficher force
 // @description  Afficher la force d'une grille de mots flêchés 20Minutes, LCI, Telestar, ...
 // @namespace    http://taoufix.com/20min-motsfleches-afficher-force
-// @version      1.2.3
+// @version      1.3.0
 // @author       taoufix
 // @match        http*://rcijeux.fr/game/*/mfleches?id=*
 // @match        http*://www.rcijeux.fr/game/*/mfleches?id=*
@@ -60,14 +60,14 @@
     $(".app").css("max-width", "880px");
     $(".game").css("padding", "15px !important");
 
+    // Add definition box
     $('body').append('<div id="top-def" style=position:absolute;top:0;right:0;background-color:black;color:white;'+
-                     'font-family:sans-serif;height:108px;width:110px;font-size:18px;text-align:center;'+
+                     'font-family:sans-serif;height:108px;width:110px;font-size:17px;text-align:center;'+
                      'display:flex;flex-direction:column;justify-content:center;align-items:center;"></div>');
 
     $("#top-def").click(function(){
         $("#top-def").hide();
     });
-
     var originalHighlightNode = window.rci.Cells.Definition.prototype.highlightNode;
     window.rci.Cells.Definition.prototype.highlightNode = function() {
         // Execute the original method
@@ -85,5 +85,23 @@
         // return the original result
         return result;
     };
+
+    // Add piano button
+    $('body').append('<div id="piano" style=position:absolute;top:5px;left:0;'+
+                   'font-size:24px;text-align:center;cursor:pointer;'+
+                     'display:flex;flex-direction:column;justify-content:center;align-items:center;">🎹</div>');
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    var ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    $("#piano").click(async function(){
+        var gm = window.rci.gameInstances.get('gameManager');
+        for (var i in ALPHABET) {
+            await sleep(50);
+            gm.inputValue(ALPHABET[i]);
+        }
+    });
 
 })();
