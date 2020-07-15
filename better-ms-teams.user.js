12e33f33f2eec32b8b34694601314aff086972c7
Taoufik El Aoumari <taoufix@gmail.com>
2020-07-15 09:30:57 +0200

    Update better-ms-teams.user.js

--- better-ms-teams.user.js
+++ better-ms-teams.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Better MS Teams
 // @namespace    http://github.com/taoufix/better-ms-teams
-// @version      1.0
+// @version      1.0.1
 // @description  Better MS Teams
 // @author       Taoufix
 // @match        https://teams.microsoft.com/*
@@ -23,6 +23,16 @@ function log(str, elm) {
 (function() {
     'use strict';
 
+    // Disable Call short-cut (Ctrl-Shift-C)
+    function disabelCallShortCut(e) {
+       if (e.ctrlKey && e.shiftKey && e.which === 67) {
+            document.location.href = 'http://www.google.com'
+        }
+    }
+    document.onkeydown = disabelCallShortCut;
+    document.onkeyup = disabelCallShortCut
+    
+    // Hightlight safe and unsafe threads
     function tweak() {
         const threadId = new URL('http://teams' + window.location.hash.substr(1)).searchParams.get('threadId');
         const div = document.getElementById('page-content-wrapper');
